4cdeff1c82483fa21b091c56acc17f805aa8cc08
krehicbenjamin <krehicbenjamin@gmail.com>
2021-06-03 16:06:48 +0200

    Setting up fragments

--- app/src/main/java/com/example/mobileproject/MainPageActivity.java
+++ app/src/main/java/com/example/mobileproject/MainPageActivity.java
@@ -1,14 +1,59 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPageActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener );
+        viewPager = findViewById(R.id.fragment_container);
+        setUpAdapter(viewPager);
     }
+
+    private void setUpAdapter(ViewPager viewPager ){
+        ViewPageAdapter viewPageAdapter = new ViewPageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT );
+        viewPageAdapter.addFragment(new HomeFragment());
+        viewPageAdapter.addFragment(new AdminFragment());
+        viewPageAdapter.addFragment(new ProfileFragment());
+
+        viewPager.setAdapter(viewPageAdapter);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Log.d("Debug", "Item clicked" + item.getItemId());
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    viewPager.setCurrentItem(0);
+                    return true;
+
+                case R.id.nav_admin:
+                    viewPager.setCurrentItem(1);
+                    return true;
+
+                case R.id.nav_profile:
+                    viewPager.setCurrentItem(2);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    };
 }
