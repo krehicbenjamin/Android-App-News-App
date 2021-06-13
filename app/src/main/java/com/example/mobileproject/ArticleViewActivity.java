41e4de8bc8fe1c55611731b482eb6d5f868ed485
BasilBosnjak <basil.bosnjak@gmail.com>
2021-06-13 16:02:31 +0200

    Bismillah

--- app/src/main/java/com/example/mobileproject/ArticleViewActivity.java
+++ app/src/main/java/com/example/mobileproject/ArticleViewActivity.java
@@ -13,7 +13,10 @@ public class ArticleViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_article_view);
         Bundle extras = getIntent().getExtras();
 
-        long id = extras.getLong(MainPageActivity.ARTICLE_EXTRA_ID);
+        title = findViewById(R.id.article_title);
+        body = findViewById(R.id.article_body);
+
+        long id = extras.getLong(HomeFragment.ARTICLE_EXTRA_ID);
         Articles articles = GolazoDatabase.getInstance(this).articleDao().getArticleById(id);
         title.setText(articles.getTitle());
         body.setText(articles.getBody());
--- app/src/main/java/com/example/mobileproject/HomeFragment.java
+++ app/src/main/java/com/example/mobileproject/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.mobileproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,31 +19,45 @@ import java.util.List;
 public class HomeFragment extends Fragment{
     public static final String ARTICLE_EXTRA_ID = "extra_id";
     private ListView listView;
+    private Context context;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        listView = view.findViewById(R.id.list_view_container);
-        return view;
+        context = container.getContext();
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        List<Articles> articles  = GolazoDatabase.getInstance(getActivity()).articleDao().getAll();
-        ArticleListAdapter adapter = new ArticleListAdapter(getActivity(), articles);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                long articleId = parent.getItemIdAtPosition(position);
-                Intent intent = new Intent (getActivity(), ArticleViewActivity.class);
-                intent.putExtra(ARTICLE_EXTRA_ID,articleId);
-                startActivity(intent);
-            }
-        }));
+        listView = view.findViewById(R.id.list_view_container);
+//        List<Articles> articles  = GolazoDatabase.getInstance(getActivity()).articleDao().getAll();
+//        ArticleListAdapter adapter = new ArticleListAdapter(getActivity(), articles);
+//        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener((new AdapterView.OnItemClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                long articleId = parent.getItemIdAtPosition(position);
+//                Intent intent = new Intent (getActivity(), ArticleViewActivity.class);
+//                intent.putExtra(ARTICLE_EXTRA_ID,articleId);
+//                startActivity(intent);
+//            }
+//        }));
+        setUpListAdapter();
     }
 
+    public void setUpListAdapter () {
+        List<Articles> todos = GolazoDatabase.getInstance(context).articleDao().getAll();
+        ArticleListAdapter adapter = new ArticleListAdapter(context, todos);
 
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+
+            long postId = parent.getItemIdAtPosition(position);
+            Intent intent = new Intent(getActivity(), ArticleViewActivity.class);
+            intent.putExtra(ARTICLE_EXTRA_ID, postId);
+            startActivity(intent);
+        });
+    }
 }
